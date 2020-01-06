72dead2076a555018f2b69cd6f0bcfa303c5fe72
Bobur <bobur.hu0991@gmail.com>
2020-01-06 14:41:24 -0500

    form validation working

---
+++ form_validation.js
@@ -0,0 +1,9 @@
+function formValidation(inputPassword) {
+    let validPassword = "12345678";
+    if(!inputPassword.value.match(validPassword))
+        alert("Invalid password!");
+    else{
+        alert("Success!");
+    }
+        
+}
